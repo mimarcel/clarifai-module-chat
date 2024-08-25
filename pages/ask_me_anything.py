e60b4c3e1310a5c098c4b31d73805cd904f50034
mimarcel <2362026+mimarcel@users.noreply.github.com>
2024-08-26 00:16:31 +0200

    Set container height

--- pages/ask_me_anything.py
+++ pages/ask_me_anything.py
@@ -26,7 +26,7 @@ detail = st.checkbox("Detail Answers",
                      help="Request the assistant to provide more details when providing answers",
                      value=True)
 
-messages = st.container()
+messages = st.container(height=200)
 if prompt := st.chat_input("Ask something"):
     messages.chat_message("user").write(prompt)
     if detail:
